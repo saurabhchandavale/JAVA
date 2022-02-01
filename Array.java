bc7d1d91d71c11c3b6eb1620783a8ef3981154f4
saurabhchandavale <85121287+saurabhchandavale@users.noreply.github.com>
2022-02-01 16:01:17 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,24 @@
+package p;
+
+public class Array {
+    public static void main(String[] args) {
+   String user[] = {"Alex","James","Mia"};
+   // int arr[] = {1,2,3};
+//    int arr[] = new int[5];
+//      arr[0] = 1;
+//      arr[1] = 2;
+//      arr[2] = 3;
+//      arr[3] = 4;
+//      arr[4] = 5;
+//for(int i = 0; i < arr.length; i++){
+//    System.out.println(arr[i]);
+//}
+//        for(int elements : arr){
+//            System.out.println(elements);
+//     }
+       for(String elements : user){
+           System.out.println(elements);
+       }
+    }
+}
+
